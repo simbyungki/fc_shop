bb5109c6535d17ad9a15d75f41b481edd53faaa2
simbyungki <dung8524@gmail.com>
2021-12-15 00:42:10 +0900

    update

--- fc_django/order/models.py
+++ fc_django/order/models.py
@@ -8,7 +8,7 @@ class Order(models.Model) :
 	register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
 	
 	def __str__(self) : 
-		return self.
+		return self.fcuser + ' ' + self.product
 
 	class Meta : 
 		db_table = 'fastcampus_order'
--- fc_django/product/models.py
+++ fc_django/product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model) : 
 	name = models.CharField(max_length=256, verbose_name='상품명')
-	price = models.CharField(max_length=128, verbose_name='상품가격')
+	price = models.IntegerField(verbose_name='상품가격')
 	description = models.TextField(verbose_name='상품설명')
 	stuck = models.IntegerField(verbose_name='재고')
 	register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
